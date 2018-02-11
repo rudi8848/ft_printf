32b2e878a84d3324423c292b39a1a42165a407f8
rudi8848 <anna.v8848@gmail.com>
2018-02-11 21:33:01 +0200

    deleted unused

--- ft_printf.c
+++ ft_printf.c
@@ -180,60 +180,6 @@ size_t		ft_printf_putchar(char **fmt, va_list *args, t_options *options, int *re
 /*
 **------------------------------------- NUMBER CONVERSIONS ------------------------------------------------
 */
-
-/*
-//----------------------itoa_base------------------------
-int		ft_nbr_len(int value, int base)
-{
-	int			len;
-	unsigned	nbr;
-
-	len = 0;
-	if (value == 0)
-		return (1);
-	else if (value < 0)
-	{
-		if (base == 10)
-			len++;
-		nbr = -value;
-	}
-	else
-		nbr = value;
-	while (nbr)
-	{
-		nbr /= base;
-		len++;
-	}
-	return (len);
-}
-
-char	*ft_itoa_base(int value, int base)
-{
-	int				len;
-	unsigned int	res;
-	char			*nbr;
-
-	len = ft_nbr_len(value, base);
-	if (value < 0)
-		res = -value;
-	else
-		res = value;
-	if (!(nbr = (char*)malloc(sizeof(char) * len + 1)))
-		return (NULL);
-	nbr[len] = '\0';
-	while (len-- > 0)
-	{
-		nbr[len] = (res % base) + (res % base > 9 ? 'A' - 10 : '0');		//<<<--- '?' forbidden by Norm
-		res /= base;
-	}
-	if (value < 0 && base == 10)
-		nbr[0] = '-';
-	return (nbr);
-}
-//-----------------------------------------------------------
-*/
-
-int	ft_nbr_length(t_number n,int  base, t_options *options)
 {
 	int len;
 	unsigned nbr;
